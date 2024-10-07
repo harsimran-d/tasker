2dc01950915503492244fb67de941522ccc7d3e0
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-07 22:06:57 +0530

    create a functional next auth flow with providers and sing in sign out on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/globals.css";
+import Providers from "./providers";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +7,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
-  return <>Tasker</>;
+  const session = useSession();
+
+  return (
+    <>
+      <div>{JSON.stringify(session)}</div>
+      <button onClick={() => signIn()}>Sign In</button>
+      <button onClick={() => signOut()}>Sign Out</button>
+    </>
+  );
 }
