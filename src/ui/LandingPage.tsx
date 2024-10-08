515ffbfe153280556a4f0c5cda26e4e4134a8753
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-08 21:34:16 +0530

    create the landing page component

---
+++ src/ui/LandingPage.tsx
@@ -0,0 +1,18 @@
+export default function LandingPage() {
+  return (
+    <div className="div mx-10 mt-3 justify-center space-y-6 divide-y-4 divide-blue-500 pt-3 text-center align-middle text-xl">
+      <p>
+        Tasker is a lightweight project planning tool designed for solo
+        developer cum entrepreneurs or small tightly knight teams of under 10.
+      </p>
+      <p className="pt-4">
+        It helps streamline project management in an intiutive way without much
+        know how of agile or scrum or kanban.
+      </p>
+      <p className="pt-4">
+        Much more than a project todo list and much simpler than an Jira
+        project.
+      </p>
+    </div>
+  );
+}
