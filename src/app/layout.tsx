dab28302581c05df72ed33a2906c3b6826a060a3
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-08 20:38:22 +0530

    add viewport attributes to root layout for responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "@/globals.css";
 import Providers from "./providers";
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
