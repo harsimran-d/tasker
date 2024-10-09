e47d84606eedb71cd8801112f0f1e91cf742f04d
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-09 11:15:30 +0530

    add navigation to dashboard page

--- src/ui/LandingNavBar.tsx
+++ src/ui/LandingNavBar.tsx
@@ -7,7 +7,7 @@ export default function LandingNavBar() {
   return (
     <div className="flex justify-end bg-blue-500 p-4 text-xl text-white">
       {session.status == "authenticated" ? (
-        <button>Dashboard</button>
+        <button onClick={() => router.push("/dashboard")}>Dashboard</button>
       ) : (
         <button onClick={() => router.push("/signup")}>Get Started</button>
       )}
