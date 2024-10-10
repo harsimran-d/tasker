258f753df2a53600a24356bb6fb55e7e8887ab90
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-10 10:43:35 +0530

    apply basic styles to signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -46,23 +46,31 @@ export default function SignupPage() {
 
   return (
     <div>
-      <h1>Signup</h1>
-      <form onSubmit={handleSignup} aria-live="polite">
+      <form
+        onSubmit={handleSignup}
+        aria-live="polite"
+        className="mt-4 flex flex-col items-center justify-center space-y-3"
+      >
+        <h1 className="text-2xl">Create Account</h1>
         <input
+          className="input-base"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          placeholder="Name"
+          placeholder="Username"
           required
           aria-describedby="name-error"
         />
-        {formErrors.username && (
+        {formErrors.username.length == 0 ? (
           <p id="name-error" style={{ color: "red" }}>
-            {formErrors.username.join(", ")}
+            {formErrors.username.join(",")}
           </p>
+        ) : (
+          ""
         )}
 
         <input
+          className="input-base"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
@@ -71,10 +79,11 @@ export default function SignupPage() {
           aria-describedby="password-error"
         />
         <input
+          className="input-base"
           type="password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder="Password"
+          placeholder="ConfirmPassword"
           required
           aria-describedby="password-error"
         />
@@ -84,8 +93,12 @@ export default function SignupPage() {
           </p>
         )}
 
-        <button type="submit" disabled={loading}>
-          {loading ? "Signing up..." : "Signup"}
+        <button
+          type="submit"
+          disabled={loading}
+          className="w-32 rounded-lg bg-blue-500 py-2 text-white"
+        >
+          {loading ? "Signing up..." : "Sign Up"}
         </button>
       </form>
       {error && (
